aa0332ca3dc976d6d1b61294a326562a258cddd0
Игорь Смирнов <rhcp-13@yandex.ru>
2022-10-19 21:50:29 +0300

    Added dockstrings, package asciimatic

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+"""Игра."""
+
 
 def attack(char_name: str, char_class: str):
+    """Расчет атаки."""
     if char_class == 'warrior':
         return (f'{char_name}'
                 f' нанёс урон противнику равный {5 + randint(3, 5)}')
@@ -14,6 +17,7 @@ def attack(char_name: str, char_class: str):
 
 
 def defence(char_name, char_class):
+    """Расчет защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +27,7 @@ def defence(char_name, char_class):
 
 
 def special(char_name, char_class):
+    """Расчет спец.умения."""
     if char_class == 'warrior':
         return (f'{char_name}'
                 f' применил специальное умение «Выносливость {80 + 25}»')
@@ -35,6 +40,7 @@ def special(char_name, char_class):
 
 
 def start_training(char_name, char_class):
+    """Тренировка персонажа."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +66,7 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
+    """Выбор персонажа."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -83,7 +90,8 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
